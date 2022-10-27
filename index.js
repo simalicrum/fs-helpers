7f9a2838415aa4664b6c0bb5e10a9ea9fbf52904
Robert Reinert <robert.reinert@gmail.com>
2022-10-27 14:17:14 -0700

    First commit

---
+++ index.js
@@ -0,0 +1,18 @@
+import fs from 'fs';
+import path from 'path';
+
+export const getAllFiles = (dirPath, arrayOfFiles) => {
+  let files = fs.readdirSync(dirPath);
+
+  arrayOfFiles = arrayOfFiles || [];
+
+  files.forEach(function (file) {
+    if (fs.statSync(dirPath + "/" + file).isDirectory()) {
+      arrayOfFiles = getAllFiles(dirPath + "/" + file, arrayOfFiles);
+    } else {
+      arrayOfFiles.push(path.join(dirPath, "/", file));
+    }
+  });
+
+  return arrayOfFiles;
+};
